db9e124354f0611e27f42421920287e549d09cc8
Mikolaj Makoszewski <S00254903@atu.ie>
2026-02-26 19:23:38 +0000

    created basic connection functions and computer object

--- ESP32HardwareMonitor/Program.cs
+++ ESP32HardwareMonitor/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace ESP32HardwareMonitor
+﻿using System.IO.Ports;
+using System;
+using System.Threading;
+using LibreHardwareMonitor.Hardware;
+
+namespace ESP32HardwareMonitor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SerialPort port = new SerialPort("COM3", 115200);
+
+            try
+            {
+                port.Open();
+                Console.WriteLine("Connected to ESP32 on COM3");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error opening port: {ex.Message}");
+                return;
+            }
+
+            Computer computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true
+            };
         }
     }
 }
