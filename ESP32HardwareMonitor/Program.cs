b7e0281282dffa8867f769452f93b2d93a9cbe94
Mikolaj Makoszewski <S00254903@atu.ie>
2026-02-26 19:20:34 +0000

    Add project files.

---
+++ ESP32HardwareMonitor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ESP32HardwareMonitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
